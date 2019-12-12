9056b12c0064fbe8d84f1154457389882c8dfc4c
Jennie Van <missjenvan@gmail.com>
2019-12-12 15:19:25 -0800

    added recipeRouter

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
+const recipeRouter = require('./recipes/recipe-router.js');
+
 const server = express();
 
 server.use(express.json());
+server.use('api/recipes', recipeRouter);
 
 server.get('/', (req, res) => {
     res.send(`<h2>Hello from the Recipes API</h2>`)
